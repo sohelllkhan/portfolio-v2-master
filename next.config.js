a444a675501bd29b996af85f6697249710eea139
sohelllkhan <sohelkhan323526@gmail.com>
2025-07-14 20:11:14 +0600

    new commit

--- next.config.js
+++ next.config.js
@@ -1,29 +1,16 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//     ],
-//   },
-//   experimental: {
-//     serverActions: true,
-//   },
-// };
-
-// module.exports = nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true, // disable Next.js image optimization
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
   },
   experimental: {
-     serverActions: true,
-   },
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
